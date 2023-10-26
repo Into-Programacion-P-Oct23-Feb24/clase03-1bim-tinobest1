b4cd0714a3a106fb469e8f9fb88a9ec31aac9dc3
TinoBest <sayayinlenin@hotmail.com>
2023-10-26 16:40:58 -0500

    copyjava

---
+++ ejemplos-IDE/Ejemplo09/src/Ejemplo11.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo11 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo 2");
+    }
+}
